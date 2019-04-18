8720c1d24fecb3823481df649239f39cd243226f
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-04-18 17:27:43 +0300

    Move `find_index.js` into `find.js`

--- packages/undercut/src/pull/operations/find.js
+++ packages/undercut/src/pull/operations/find.js
@@ -13,3 +13,21 @@ export function find(predicate) {
 		}
 	};
 }
+
+export function findIndex(predicate) {
+	assertPredicate(predicate);
+
+	return function* (iterable) {
+		let index = 0;
+
+		for (const item of iterable) {
+			if (predicate(item)) {
+				yield index;
+
+				return;
+			}
+
+			index += 1;
+		}
+	};
+}
--- packages/undercut/src/pull/operations/find.test.js
+++ packages/undercut/src/pull/operations/find.test.js
@@ -1,6 +1,6 @@
 import { targetOf } from "../../utils/tests.js";
 
-import { find } from "./find.js";
+import { find, findIndex } from "./find.js";
 
 test("find", () => {
 	expect(() => find()).toThrow();
@@ -21,4 +21,16 @@ test("find", () => {
 	];
 
 	expect(targetOf(find(u => u.name === "b"), users)).toEqual([users[2]]);
+});
+
+test("findIndex", () => {
+	expect(() => findIndex()).toThrow();
+
+	const predicate = x => x === 2;
+
+	expect(targetOf(findIndex(predicate), [])).toEqual([]);
+	expect(targetOf(findIndex(predicate), [1])).toEqual([]);
+	expect(targetOf(findIndex(predicate), [2, 5])).toEqual([0]);
+	expect(targetOf(findIndex(predicate), [1, 0, 2, 5])).toEqual([2]);
+	expect(targetOf(findIndex(predicate), [1, -3, -5, 2])).toEqual([3]);
 });
