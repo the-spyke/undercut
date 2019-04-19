cfb63c15c7c6c05891410bb19b6507368a63c034
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-04-19 17:38:27 +0300

    Fix missing exports

--- packages/undercut/src/index.js
+++ packages/undercut/src/index.js
@@ -1,7 +1,2 @@
-import * as compare from "./utils/compare.js";
-
-export { compare };
-export * from "./utils/function.js";
-export * from "./utils/helpers.js";
-export * from "./utils/lang.js";
+export * from "./utils.js";
 export * from "./pull.js";
