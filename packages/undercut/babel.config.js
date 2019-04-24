e85acb16e43795ae8f85096e3b8b949385370354
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-04-24 14:01:38 +0300

    Add comment about Jest and ESM

--- packages/undercut/babel.config.js
+++ packages/undercut/babel.config.js
@@ -5,6 +5,7 @@ const presets = [
 		"@babel/preset-env",
 		{
 			corejs: 3,
+			// Jest doesn't support ES Modules on Node because of custom `require()` hooks.
 			modules: "commonjs",
 			targets: {
 				node: "current"
