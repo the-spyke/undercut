8c61ca07b73c007f73dee94c0042eceb7931017b
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-10-23 15:43:32 +0300

    Add exports test

---
+++ packages/undercut/src/exports.test.js
@@ -0,0 +1,22 @@
+import * as indexExports from "./index.js";
+import * as pullExports from "./pull.js";
+import * as pushExports from "./push.js";
+import * as utilsExports from "./utils.js";
+
+describe(`exports`, () => {
+	test(`index`, () => {
+		expect(Object.keys(indexExports).sort()).toMatchSnapshot();
+	});
+
+	test(`pull`, () => {
+		expect(Object.keys(pullExports).sort()).toMatchSnapshot();
+	});
+
+	test(`push`, () => {
+		expect(Object.keys(pushExports).sort()).toMatchSnapshot();
+	});
+
+	test(`utils`, () => {
+		expect(Object.keys(utilsExports).sort()).toMatchSnapshot();
+	});
+});
