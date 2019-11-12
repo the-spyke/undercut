85786e0763f3d6d46881e8b3033f351d34ae8150
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-11-12 17:02:41 +0300

    Do not use Babel for polyfills

--- packages/undercut/babel.config.cjs
+++ packages/undercut/babel.config.cjs
@@ -4,13 +4,11 @@ const presets = [
 	[
 		`@babel/preset-env`,
 		{
-			corejs: 3,
 			// Jest doesn't support ES Modules on Node because of custom `require()` hooks.
 			modules: `commonjs`,
 			targets: {
 				node: `current`
-			},
-			useBuiltIns: `entry`
+			}
 		}
 	]
 ];
