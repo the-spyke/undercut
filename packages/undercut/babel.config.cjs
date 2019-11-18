82ee98112ade02f867fc0018b891be79416626da
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-11-18 14:48:41 +0300

    Improve package scripts

--- packages/undercut/babel.config.cjs
+++ packages/undercut/babel.config.cjs
@@ -1,18 +1,18 @@
 /* eslint-env node */
 
-const presets = [
-	[
-		`@babel/preset-env`,
-		{
-			// Jest doesn't support ES Modules on Node because of custom `require()` hooks.
-			modules: `commonjs`,
-			targets: {
-				node: `current`
-			}
-		}
-	]
-];
+const IS_TEST_ENV = process.env.NODE_ENV === `test`;
 
 module.exports = {
-	presets
+	presets: [
+		[
+			`@babel/preset-env`,
+			{
+				// Jest doesn't support ES Modules on Node because of custom `require()` hooks.
+				modules: IS_TEST_ENV ? `commonjs` : false,
+				targets: {
+					node: `current`
+				}
+			}
+		]
+	]
 };
