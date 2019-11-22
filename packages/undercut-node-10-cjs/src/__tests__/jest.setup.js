e16881b68b74cc64dfed08bb9888b0adbac6e5da
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-11-22 14:28:26 +0300

    Build node package files with Babel too

--- packages/undercut-node-10-cjs/src/__tests__/jest.setup.js
+++ packages/undercut-node-10-cjs/src/__tests__/jest.setup.js
@@ -1,2 +1,2 @@
 // Polyfills for Node 10
-require(`core-js/es`);
+import "core-js/es";
--- packages/undercut-node-10-cjs/src/__tests__/node.test.js
+++ packages/undercut-node-10-cjs/src/__tests__/node.test.js
@@ -1,4 +1,4 @@
-const satisfies = require(`semver`).satisfies;
+import { satisfies } from "semver";
 
 test(`Node.js is version 10.x`, () => {
 	expect(satisfies(process.version, `^10.x`)).toBe(true);
