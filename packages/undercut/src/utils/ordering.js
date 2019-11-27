609dc7d89862b4cd4062d0a6dffc1b3fdaadcaad
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-11-27 17:22:34 +0300

    Unify ordering specs

--- packages/undercut/src/utils/ordering.js
+++ packages/undercut/src/utils/ordering.js
@@ -1,25 +1,25 @@
 import { assert, assertFunctor } from "./assert.js";
 import { identity } from "./function.js";
-import { isFunction, isNumberValue } from "./language.js";
+import { isFunction } from "./language.js";
 
 export function orderingFactory(specs) {
 	assert(Array.isArray(specs), `"specs" must be an array of ordering specs produced by asc/desc functions.`);
 	specs.forEach((spec, index) => {
 		assert(
-			Array.isArray(spec) && spec.length === 3 && isFunction(spec[0]) && isFunction(spec[1]) && isNumberValue(spec[2]),
+			Array.isArray(spec) && spec.length === 3 && (spec[0] === asc || spec[0] === desc) && isFunction(spec[1]) && isFunction(spec[2]),
 			`#${index} ordering spec is invalid.`
 		);
 	});
 
 	return function (a, b) {
-		for (const [comparator, selector, order] of specs) {
+		for (const [order, comparator, selector] of specs) {
 			const x = selector(a);
 			const y = selector(b);
 
-			const result = order * comparator(x, y);
+			const result = comparator(x, y);
 
 			if (result !== 0) {
-				return result;
+				return order === asc ? result : -result;
 			}
 		}
 
@@ -31,12 +31,12 @@ export function asc(comparator, selector = identity) {
 	assertFunctor(comparator, `comparator`);
 	assertFunctor(selector, `selector`);
 
-	return [comparator, selector, 1];
+	return [asc, comparator, selector];
 }
 
 export function desc(comparator, selector = identity) {
 	assertFunctor(comparator, `comparator`);
 	assertFunctor(selector, `selector`);
 
-	return [comparator, selector, -1];
+	return [desc, comparator, selector];
 }
--- packages/undercut/src/utils/ordering.test.js
+++ packages/undercut/src/utils/ordering.test.js
@@ -7,14 +7,14 @@ test(`asc`, () => {
 	const selector = x => x;
 
 	expect(() => asc()).toThrow();
-	expect(asc(compare.numbers)).toEqual([compare.numbers, identity, 1]);
-	expect(asc(compare.numbers, selector)).toEqual([compare.numbers, selector, 1]);
+	expect(asc(compare.numbers)).toEqual([asc, compare.numbers, identity]);
+	expect(asc(compare.numbers, selector)).toEqual([asc, compare.numbers, selector]);
 });
 
 test(`desc`, () => {
 	const selector = x => x;
 
 	expect(() => desc()).toThrow();
-	expect(desc(compare.numbers)).toEqual([compare.numbers, identity, -1]);
-	expect(desc(compare.numbers, selector)).toEqual([compare.numbers, selector, -1]);
+	expect(desc(compare.numbers)).toEqual([desc, compare.numbers, identity]);
+	expect(desc(compare.numbers, selector)).toEqual([desc, compare.numbers, selector]);
 });
