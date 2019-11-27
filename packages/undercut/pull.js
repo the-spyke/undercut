99811ee23868a6c58298eb064d8e571e33af2a81
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-11-27 17:23:45 +0300

    Remove iterable/observer utils from pull/push entries

--- packages/undercut/pull.js
+++ packages/undercut/pull.js
@@ -2,5 +2,3 @@ export * from "./src/pull/pull_core.js";
 export * from "./src/pull/pull_operations.js";
 export * from "./src/pull/pull_sources.js";
 export * from "./src/pull/pull_targets.js";
-
-export * from "./src/utils/iterable.js";
