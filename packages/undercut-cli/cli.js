23bbbf1c67a61d61f3cd947d32bb74016184de3e
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-12-09 15:10:56 +0300

    Add `@undercut/cli` prototype

---
+++ packages/undercut-cli/cli.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+// Polyfills first
+import "core-js/es";
+
+import yargs from "yargs";
+
+import { runPush } from "./index.js";
+
+const argv = yargs
+	.scriptName(`undercut`)
+	.usage(`$0 [options] [operations]`)
+	.option(`sourse`, {
+		alias: `s`,
+		demandOption: false,
+		default: undefined,
+		describe: `A pull source that will be used to generate input lines instead of piping "stdin"`,
+		type: `string`
+	})
+	.help()
+	.argv;
+
+runPush(argv.sourse, argv._);
