dde7169fdbe23ba8331eab73b00eb21b01f6afd6
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-12-13 12:58:45 +0300

    Improve build infrastructure

--- packages/undercut-web-2019/jest.config.js
+++ packages/undercut-web-2019/jest.config.js
@@ -3,7 +3,12 @@
 "use strict";
 
 const webConfig = require(`@undercut/config/jest/jest.config.web.cjs`);
+const { referRootProjects } = require(`@undercut/config/jest/jest_config_helpers.cjs`);
 
 module.exports = {
 	...webConfig,
+	projects: [
+		`.`,
+		...referRootProjects()
+	]
 };
--- packages/undercut-web-2019/scripts/build.sh
+++ packages/undercut-web-2019/scripts/build.sh
@@ -3,25 +3,12 @@
 set -e
 
 CWD="$( pwd )"
-BUILD="$CWD/build"
 PATH="$CWD/node_modules/.bin:$PATH"
-OUTPUT="$BUILD/output"
-UC_DIR="$( cd ../undercut && pwd )"
-
-echo "CWD: $CWD"
+BUILD="$CWD/build"
 
 rm -rf $BUILD
 mkdir $BUILD
-mkdir $OUTPUT
 
-babel $UC_DIR/src --out-dir $OUTPUT/src
-babel $UC_DIR/*.js --out-dir $OUTPUT
-babel src --out-dir $OUTPUT/src
-
-cd $UC_DIR
-find src -name '*.js.snap' -exec cp -v --parents '{}' $OUTPUT \;
-
-cd $CWD
 cp -vr LICENSE package.json README.md $BUILD
 
 webpack
