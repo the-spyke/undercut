211f42e5ab7cfe364df5aa753f1f61d5520575a9
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-12-16 15:54:00 +0300

    Add testing exports test

--- packages/undercut-tests/src/exports.test.js
+++ packages/undercut-tests/src/exports.test.js
@@ -1,17 +1,22 @@
 import * as pullExports from "@undercut/pull";
 import * as pushExports from "@undercut/push";
+import * as testingExports from "@undercut/testing";
 import * as utilsExports from "@undercut/utils";
 
 describe(`exports`, () => {
-	test(`pull`, () => {
+	test(`@undercut/pull`, () => {
 		expect(Object.keys(pullExports).sort()).toMatchSnapshot();
 	});
 
-	test(`push`, () => {
+	test(`@undercut/push`, () => {
 		expect(Object.keys(pushExports).sort()).toMatchSnapshot();
 	});
 
-	test(`utils`, () => {
+	test(`@undercut/testing`, () => {
+		expect(Object.keys(testingExports).sort()).toMatchSnapshot();
+	});
+
+	test(`@undercut/utils`, () => {
 		expect(Object.keys(utilsExports).sort()).toMatchSnapshot();
 	});
 });
