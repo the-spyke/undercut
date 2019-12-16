8b3de9f2ad467a73d163bafa6248071c57a35601
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-12-16 13:57:35 +0300

    Improve CLI build, add tests

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+rm -rf coverage
 rm -rf node_modules
 rm -rf packages/*/build
 rm -rf packages/*/coverage
 rm -rf packages/*/dist
 rm -rf packages/*/node_modules
-rm -rf packages/*/*.tgz
+rm -f packages/*/*.tgz
 rm -f *.tgz
