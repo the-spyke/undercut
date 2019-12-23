429a43e817efd489631e05c021c5d2af2b3f74a0
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2019-12-23 16:58:17 +0300

    Refactor flatten and upgrade its tests

--- packages/undercut-pull/src/operations/flatten.js
+++ packages/undercut-pull/src/operations/flatten.js
@@ -1,25 +1,23 @@
 import { assert } from "@undercut/utils/src/assert.js";
 import { isIterable, isPositiveOrZero } from "@undercut/utils/src/language.js";
 
-function* spreadRec(canSpread, maxDepth, currentDepth, value) {
-	if (currentDepth < maxDepth && canSpread(value)) {
-		for (const item of value) {
-			yield* spreadRec(canSpread, maxDepth, currentDepth + 1, item);
+function* flattenRec(canFlatten, maxDepth, currentDepth, iterable) {
+	for (const item of iterable) {
+		if (currentDepth < maxDepth && canFlatten(item)) {
+			yield* flattenRec(canFlatten, maxDepth, currentDepth + 1, item);
+		} else {
+			yield item;
 		}
-	} else {
-		yield value;
 	}
 }
 
-function flattenCore(canSpread, depth) {
+function flattenCore(canFlatten, depth) {
 	assert(isPositiveOrZero(depth), `"depth" is required, must be a number >= 0.`);
 
 	depth = Math.trunc(depth);
 
-	return function* (iterable) {
-		for (const item of iterable) {
-			yield* spreadRec(canSpread, depth, 0, item);
-		}
+	return function (iterable) {
+		return flattenRec(canFlatten, depth, 0, iterable);
 	};
 }
 
