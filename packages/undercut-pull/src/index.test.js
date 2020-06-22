52063ba59e27dd40d8fa324ac31f6ca6a370bd26
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2020-06-22 17:52:51 +0300

    tests: update exports tests names

--- packages/undercut-pull/src/index.test.js
+++ packages/undercut-pull/src/index.test.js
@@ -3,7 +3,7 @@ import { describe, expect, test } from "@jest/globals";
 import * as exports from "./index.js";
 
 describe(`Exports`, () => {
-	test(`of the main entry should be stable`, () => {
+	test(`of the main "pull" entry should be stable`, () => {
 		expect(Object.keys(exports).sort()).toMatchSnapshot();
 	});
 });
