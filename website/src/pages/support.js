a0647161984ffb081a29d3c92537bad7296cd00f
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2020-06-25 16:57:28 +0300

    chore(website): improve support page links

--- website/src/pages/support.js
+++ website/src/pages/support.js
@@ -16,17 +16,17 @@ export default function Support() {
 						<h1>Need Help?</h1>
 
 						<h2>Ask a question</h2>
-						<p>You may open an issue with label &quot;Issue: Question&quot; on <a href="https://github.com/the-spyke/undercut/issues">GitHub</a>.</p>
+						<p>You may open an issue with label &quot;Issue: Question&quot; on <a href="https://github.com/the-spyke/undercut/issues" target="blank">GitHub</a>.</p>
 
 						<h2>Twitter</h2>
-						<p><a href="https://twitter.com/hashtag/undercutjs">#undercutjs</a> hash tag on Twitter is used to keep up with the latest Undercut news.</p>
+						<p>Use <a href="https://twitter.com/hashtag/undercutjs" target="blank">#undercutjs</a> hash tag on <b>Twitter</b>. You may also ask <a href="https://twitter.com/the_spyke" target="blank">@the_spyke</a> here directly.</p>
 
 						<h2>DEV Community</h2>
-						<p>Use <a href="https://dev.to/t/undercutjs">#undercutjs</a> tag on <b>DEV.to</b> to find posts, ask for help, or just discuss Undercut.</p>
+						<p>Use <a href="https://dev.to/t/undercutjs/latest" target="blank">#undercutjs</a> tag on <b>DEV.to</b> to find posts, ask for help, or just discuss Undercut.</p>
 
 						<h2>License</h2>
 						<p>
-							The code is licensed under the <a rel="license" href="https://github.com/the-spyke/undercut/blob/master/LICENSE">MIT License</a>.
+							The code is licensed under the <a rel="license" href="https://github.com/the-spyke/undercut/blob/master/LICENSE" target="blank">MIT License</a>.
 						</p>
 					</div>
 				</div>
