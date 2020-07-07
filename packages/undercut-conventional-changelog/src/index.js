fa662f2acd40e837f6e98336441d5c8ae9d891c5
Anton Alexandrenok <anton.alexandrenok@gmail.com>
2020-07-07 16:21:54 +0300

    chore: make preset factory regular function because lerna detection has a bug

--- packages/undercut-conventional-changelog/src/index.js
+++ packages/undercut-conventional-changelog/src/index.js
@@ -4,15 +4,14 @@ const getParserOpts = require(`./get_parser_opts.js`);
 const getWriterOpts = require(`./get_writer_opts.js`);
 const getRecommendedBumpOpts = require(`./get_recommended_bump_opts.js`);
 
-module.exports = async function getUndercutPreset(config = {}) {
+module.exports = function getUndercutPreset(config = {}) {
 	const parserOpts = getParserOpts(config);
 	const recommendedBumpOpts = getRecommendedBumpOpts(config);
-	const writerOpts = await getWriterOpts(config);
 
-	return {
+	return getWriterOpts(config).then(writerOpts => ({
 		conventionalChangelog: { parserOpts, writerOpts },
 		parserOpts,
 		recommendedBumpOpts,
 		writerOpts,
-	};
+	}));
 };
