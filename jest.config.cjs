9b36a3a6e2b066f19cd740f0d98d37d55a2878c4
Anton Alexandrenok <antonalexandrenok@gmail.com>
2020-12-31 17:07:48 +0300

    chore: Replace changelog preset with external package

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,7 +5,6 @@ module.exports = {
 		// `<rootDir>/packages/undercut-cli/`, -- Should be tested from a Node 10 environment.
 		`<rootDir>/packages/undercut-collections/`,
 		// `<rootDir>/packages/undercut-config/`, -- Should have no tests.
-		// `<rootDir>/packages/undercut-conventional-changelog-preset/`, -- Test as a separate job or test:changelog script.
 		// `<rootDir>/packages/undercut-node-10/`, -- Should be tested from a Node 10 environment.
 		// `<rootDir>/packages/undercut-perf/`, -- Performance tests must be in a separate job (very slow).
 		`<rootDir>/packages/undercut-pull/`,
--- packages/undercut-conventional-changelog-preset/README.md
+++
@@ -1,7 +0,0 @@
-# @undercut/conventional-changelog-preset
-
-Based on the [conventional-changelog-conventionalcommits](https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-conventionalcommits).
-
-## License
-
-Licensed under the MIT License, see [LICENSE](LICENSE) for more information.
