3b9cb2b5456824a7daa541c562062edaad7cf994
Anton Alexandrenok <antonalexandrenok@gmail.com>
2021-02-09 23:04:51 +0300

    test(node): Use dynamic import for exports tests

--- packages/undercut-node/src/__tests__/exports.test.js
+++ packages/undercut-node/src/__tests__/exports.test.js
@@ -1,29 +1,30 @@
 import { describe, expect, test } from "@jest/globals";
 
-import * as pullExports from "@undercut/pull";
-import * as pushExports from "@undercut/push";
-import * as utilsExports from "@undercut/utils";
-
-import * as myPullExports from "../../exports/pull.js";
-import * as myPushExports from "../../exports/push.js";
-import * as myUtilsExports from "../../exports/utils.js";
-
-import * as myOwnExports from "../index.js";
-
 describe(`Exports of @undercut/node`, () => {
-	test(`should be stable`, () => {
+	test(`should be stable`, async () => {
+		const myOwnExports = await import(`../index.js`);
+
 		expect(Object.keys(myOwnExports).sort()).toMatchSnapshot();
 	});
 
-	test(`/pull should match @undercut/pull`, () => {
+	test(`/pull should match @undercut/pull`, async () => {
+		const pullExports = await import(`@undercut/pull`);
+		const myPullExports = await import(`../../exports/pull.js`);
+
 		expect(Object.keys(myPullExports).sort()).toEqual(Object.keys(pullExports).sort());
 	});
 
-	test(`/push should match @undercut/push`, () => {
+	test(`/push should match @undercut/push`, async () => {
+		const pushExports = await import(`@undercut/push`);
+		const myPushExports = await import(`../../exports/push.js`);
+
 		expect(Object.keys(myPushExports).sort()).toEqual(Object.keys(pushExports).sort());
 	});
 
-	test(`/utils should match @undercut/utils`, () => {
+	test(`/utils should match @undercut/utils`, async () => {
+		const utilsExports = await import(`@undercut/utils`);
+		const myUtilsExports = await import(`../../exports/utils.js`);
+
 		expect(Object.keys(myUtilsExports).sort()).toEqual(Object.keys(utilsExports).sort());
 	});
 });
