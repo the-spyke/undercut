2740c434de4133aa9b1a7de438ddfd6bbca77a3e
Anton Alexandrenok <antonalexandrenok@gmail.com>
2021-02-11 18:09:49 +0300

    chore(web-2019): Update Jest and Babel configs

--- packages/undercut-web-2019/babel.config.cjs
+++ packages/undercut-web-2019/babel.config.cjs
@@ -9,12 +9,14 @@ module.exports = {
 		[
 			`@babel/preset-env`,
 			{
+				corejs: 3,
 				// Jest doesn't support ES Modules because of custom `require()` hooks.
 				// Webpack wants to see original ESMs.
 				modules: IS_TEST_ENV ? `commonjs` : false,
 				// EDGE 18.1 was released in 2019, but BrowserList doesn't know that.
 				// Somehow Android WebView (based on Blink) <= 77 transpile Generators.
 				targets: `since 2019, edge >= 18, not android > 0`,
+				useBuiltIns: `entry`,
 			}
 		]
 	],
