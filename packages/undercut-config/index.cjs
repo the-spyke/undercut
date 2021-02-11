a855ec1a367b1363bf4ed5a9b677cc5a4df732ae
Anton Alexandrenok <antonalexandrenok@gmail.com>
2021-02-11 17:24:07 +0300

    chore(node): Improve node package build

--- packages/undercut-config/index.cjs
+++ packages/undercut-config/index.cjs
@@ -10,6 +10,7 @@ const IS_TEST_ENV = process.env.NODE_ENV === TEST;
 const IS_DEV_ENV = !IS_PROD_ENV && !IS_TEST_ENV;
 
 const NODE_BUILD_TARGET = process.env.NODE_BUILD_TARGET || `current`;
+const NODE_LTS_VERSION = `12.17`;
 
 const IGNORE_PATTERNS = [
 	`/build/`,
@@ -26,6 +27,7 @@ module.exports = {
 	IS_PROD_ENV,
 	IS_TEST_ENV,
 	NODE_BUILD_TARGET,
+	NODE_LTS_VERSION,
 	PROD,
 	RMS,
 	TEST,
