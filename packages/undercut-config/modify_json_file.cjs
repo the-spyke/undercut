871ee9d95f1d855bb87400cd92e2b447ee5ae73d
Anton Alexandrenok <antonalexandrenok@gmail.com>
2021-02-11 16:19:44 +0300

    chore(config): Add output on json modification

--- packages/undercut-config/modify_json_file.cjs
+++ packages/undercut-config/modify_json_file.cjs
@@ -1,16 +1,25 @@
 "use strict";
 
 const { readFile, writeFile } = require(`fs`).promises;
+
+const { diffStringsUnified } = require(`jest-diff`);
 const yargs = require(`yargs`);
 
 module.exports = async function modifyJsonFile(action) {
 	const [source, target = source] = yargs.argv._;
+
+	console.log(`----> Applying JSON modification to '${source}'`); // eslint-disable-line no-console
+
 	const sourceText = await readFile(source, `utf8`);
 	const json = JSON.parse(sourceText);
 
-	action(json);
+	const result = action(json) ?? json;
 
-	const targetText = JSON.stringify(json, null, 2);
+	const targetText = JSON.stringify(result, null, 2) + `\n`;
 
-	return writeFile(target, targetText, `utf8`);
+	console.log(diffStringsUnified(sourceText, targetText, { expand: false })); // eslint-disable-line no-console
+
+	await writeFile(target, targetText, `utf8`);
+
+	console.log(`----> Sucessfully applied ${source === target ? `inplace` : `at '${target}'`}`); // eslint-disable-line no-console
 };
