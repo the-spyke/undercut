bbd0d828173629dba01bab55bc09fa62f324fd12
Anton Alexandrenok <antonalexandrenok@gmail.com>
2021-02-11 18:05:16 +0300

    test: Use jest-circus

--- packages/undercut-config/jest/jest.config.base.cjs
+++ packages/undercut-config/jest/jest.config.base.cjs
@@ -8,13 +8,16 @@ module.exports = {
 	],
 	coveragePathIgnorePatterns: IGNORE_PATTERNS,
 	coverageProvider: `v8`,
+	injectGlobals: false,
 	moduleFileExtensions: [`js`, `cjs`, `mjs`, `json`, `jsx`, `ts`, `tsx`, `node`],
 	testMatch: [
 		`**/*.test.*.?(c|m)js`,
 		`**/*.test.?(c|m)js`,
 	],
 	testPathIgnorePatterns: IGNORE_PATTERNS,
+	testRunner: `jest-circus/runner`,
 	transform: {
 		"\\.(c|m)?js$": `@undercut/config/jest/babel_jest_transformer.cjs`
 	},
+	transformIgnorePatterns: [],
 };
