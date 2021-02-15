c642bc638d7d63b397e65496a7b52218f26922fc
Anton Alexandrenok <antonalexandrenok@gmail.com>
2021-02-15 19:41:42 +0300

    chore: Convert testing to TS

--- packages/undercut-testing/src/index.test.ts
+++ packages/undercut-testing/src/index.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@jest/globals";
 
-import { simulatePull } from "./index.js";
+import { simulatePull } from "./index";
 
 test(`simulatePull`, () => {
 	expect(simulatePull(i => i, [1, 3])).toEqual([1, 3]);
