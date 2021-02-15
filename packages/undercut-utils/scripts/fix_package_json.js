c0401cb2898e73a946d42686b48702a3f47aa88d
Anton Alexandrenok <antonalexandrenok@gmail.com>
2021-02-15 16:04:58 +0300

    chore(utils)!: Add exports field and expose assert and array

--- packages/undercut-utils/scripts/fix_package_json.js
+++ packages/undercut-utils/scripts/fix_package_json.js
@@ -3,4 +3,10 @@ import { modifyJsonFile } from "@undercut/config/modify_json_file.js";
 modifyJsonFile(json => {
 	json.main = `./src/index.js`;
 	json.types = `./src/index.d.ts`;
+	json.exports = {
+		".": `./src/index.js`,
+		"./array": `./src/array.js`,
+		"./assert": `./src/assert.js`,
+		"./package.json": `./package.json`
+	};
 });
