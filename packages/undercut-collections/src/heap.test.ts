41b28f97ba20a5859c2601898dd28892492288f7
Anton Alexandrenok <antonalexandrenok@gmail.com>
2021-02-15 16:29:43 +0300

    chore: Finish converting collections to TS

--- packages/undercut-collections/src/heap.test.ts
+++ packages/undercut-collections/src/heap.test.ts
@@ -1,33 +1,40 @@
 import { describe, expect, test } from "@jest/globals";
 
-import { Heap, MaxHeap, MinHeap } from "./heap";
+import {
+	getParentIndex,
+	getLeftChildIndex,
+	getRightChildIndex,
+	Heap,
+	MaxHeap,
+	MinHeap,
+} from "./heap";
 
 describe(`Heap`, () => {
 	test(`getParentIndex should return a valid index`, () => {
-		expect(Heap.getParentIndex(0)).toBe(-1);
-		expect(Heap.getParentIndex(1)).toBe(0);
-		expect(Heap.getParentIndex(2)).toBe(0);
-		expect(Heap.getParentIndex(3)).toBe(1);
-		expect(Heap.getParentIndex(5)).toBe(2);
-		expect(Heap.getParentIndex(14)).toBe(6);
+		expect(getParentIndex(0)).toBe(-1);
+		expect(getParentIndex(1)).toBe(0);
+		expect(getParentIndex(2)).toBe(0);
+		expect(getParentIndex(3)).toBe(1);
+		expect(getParentIndex(5)).toBe(2);
+		expect(getParentIndex(14)).toBe(6);
 	});
 
 	test(`getLeftChildIndex should return a valid index`, () => {
-		expect(Heap.getLeftChildIndex(0)).toBe(1);
-		expect(Heap.getLeftChildIndex(1)).toBe(3);
-		expect(Heap.getLeftChildIndex(2)).toBe(5);
-		expect(Heap.getLeftChildIndex(3)).toBe(7);
-		expect(Heap.getLeftChildIndex(5)).toBe(11);
-		expect(Heap.getLeftChildIndex(6)).toBe(13);
+		expect(getLeftChildIndex(0)).toBe(1);
+		expect(getLeftChildIndex(1)).toBe(3);
+		expect(getLeftChildIndex(2)).toBe(5);
+		expect(getLeftChildIndex(3)).toBe(7);
+		expect(getLeftChildIndex(5)).toBe(11);
+		expect(getLeftChildIndex(6)).toBe(13);
 	});
 
 	test(`getRightChildIndex should return a valid index`, () => {
-		expect(Heap.getRightChildIndex(0)).toBe(2);
-		expect(Heap.getRightChildIndex(1)).toBe(4);
-		expect(Heap.getRightChildIndex(2)).toBe(6);
-		expect(Heap.getRightChildIndex(3)).toBe(8);
-		expect(Heap.getRightChildIndex(5)).toBe(12);
-		expect(Heap.getRightChildIndex(6)).toBe(14);
+		expect(getRightChildIndex(0)).toBe(2);
+		expect(getRightChildIndex(1)).toBe(4);
+		expect(getRightChildIndex(2)).toBe(6);
+		expect(getRightChildIndex(3)).toBe(8);
+		expect(getRightChildIndex(5)).toBe(12);
+		expect(getRightChildIndex(6)).toBe(14);
 	});
 });
 
